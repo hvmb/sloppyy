7f05b36974061a37f8ce9924555e23df4c874539
hvmb <83955253+hvmb@users.noreply.github.com>
2024-04-01 13:06:03 -0400

    Add files via upload

---
+++ Speculate-assets/typed.js
@@ -0,0 +1,7 @@
+new Typed('#typed',{
+  strings : [,'Im a Qutie','#skidded','Undownable','Fuck Pedos','Femboys on top','Yall Talk Too Much Shit','https://Sloppy.lol :D', 'I love u Nvm!', 'I love u Astrid!', 'Fuck Liam Johnson'],
+  typeSpeed : 35,
+  delaySpeed : 90,
+  showCursor: false,
+  loop : true
+})
